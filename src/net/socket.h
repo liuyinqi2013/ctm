0e284513b90a33de54314579345d5d59a2bea692
pandaliu <liuyinqi2013@163.com>
2017-12-27 22:24:29 +0800

    修改socket.h文件

--- src/net/socket.h
+++ src/net/socket.h
@@ -1,29 +1,47 @@
 #ifndef CTM_NET_SOCKET_H__
 #define CTM_NET_SOCKET_H__
 
+#ifdef WIN32
+#include <Winsock2.h>
+#define socket_t SOCKET
+#define socklen_t int
+#define sockbuf_t char
+#define socket_invalid (SOCKET)(~0)
+#define socket_error (-1)
+
+inline int CloseSocket(socket_t& sockfd)
+{
+	return closesocket(sockfd);
+}
+
+#else
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #define socket_t int
-#define INVALID_SOCKET -1;
-
-inline socket_t CreateSocket(int domain, int type, int protocol)
-{
-	return socket(domain, type, protocol);
-}
+#define socklen_t socklen_t
+#define sockbuf_t void
+#define socket_invalid (SOCKET)(~0)
+#define socket_error (-1)
 
 inline int CloseSocket(socket_t& sockfd)
 {
 	return close(sockfd);
 }
 
-inline int ShutDown(socket_t& sockfd, int how)
+#endif
+
+inline socket_t CreateSocket(int domain, int type, int protocol)
 {
-	return shutdown(sockfd, how)
+	return socket(domain, type, protocol);
 }
 
-inline socket_t Accept(socket_t sockfd, struct sockaddr* addr, int* addrlen)
+inline int ShutDown(socket_t& sockfd, int how)
+{
+	return shutdown(sockfd, how);
+}
+
+inline socket_t Accept(socket_t sockfd, struct sockaddr* addr, socklen_t* addrlen)
 {
 	return accept(sockfd, addr, addrlen);
 }
@@ -33,9 +51,20 @@ inline int Listen(socket_t sockfd, int backlog)
 	return listen(sockfd, backlog);
 }
 
-inline int Recv(int sockfd, void *buf, size_t len, int flags)
+inline int Connect(socket_t sockfd, const struct sockaddr* addr, socklen_t len)
+{
+	return connect(sockfd, addr, len);
+}
+
+inline int Send(socket_t sockfd, const sockbuf_t* buf, size_t len, int flags)
+{
+	return send(sockfd, buf, len, flags);
+}
+
+inline int Recv(socket_t sockfd, sockbuf_t* buf, size_t len, int flags) 
 {
 	return recv(sockfd, buf, len, flags);
 }
+
 
 #endif
--- src/thread/mutex.h
+++ src/thread/mutex.h
@@ -5,26 +5,20 @@
 #ifdef WIN32
 #include <Windows.h>
 #define mutex_t HANDLE 
-inline int mutex_init(mutex_t& mutex) { return ((mutex = CreateMutex(NULL, FALSE, NULL)) ? 0 : -1); }
+inline int mutex_init(mutex_t& mutex)    { return ((mutex = CreateMutex(NULL, FALSE, NULL)) ? 0 : -1); }
 inline int mutex_destroy(mutex_t& mutex) { return CloseHandle(mutex) ? 0 : -1; }
-inline int mutex_lock(mutex_t& mutex) { return WaitForSingleObject(mutex, INFINITE); }
+inline int mutex_lock(mutex_t& mutex)    { return WaitForSingleObject(mutex, INFINITE); }
 inline int mutex_trylock(mutex_t& mutex) { return 0; }
 inline int mutex_unlock(mutex_t& mutex)  { return ReleaseMutex(mutex) ? 0 : -1; }
 
 #else
 #include <pthread.h>
 #define mutex_t pthread_mutex_t
-inline int mutex_init(mutex_t& mutex) { return pthread_mutex_init(&mutex, NULL); }
+inline int mutex_init(mutex_t& mutex)    { return pthread_mutex_init(&mutex, NULL); }
 inline int mutex_destroy(mutex_t& mutex) { return pthread_mutex_init(&mutex, NULL); }
-inline int mutex_lock(mutex_t& mutex) { return pthread_mutex_lock(&mutex); }
+inline int mutex_lock(mutex_t& mutex)    { return pthread_mutex_lock(&mutex); }
 inline int mutex_trylock(mutex_t& mutex) { return pthread_mutex_trylock(&mutex); }
 inline int mutex_unlock(mutex_t& mutex)  { return pthread_mutex_unlock(&mutex); }
-
-//#define mutex_init(mutex)    pthread_mutex_init(&mutex, NULL)
-//#define mutex_destroy(mutex) pthread_mutex_destroy(&mutex)
-//#define mutex_lock(mutex)    pthread_mutex_lock(&mutex)
-//#define mutex_trylock(mutex) pthread_mutex_trylock(&mutex)
-//#define mutex_unlock(mutex)  pthread_mutex_unlock(&mutex)
 	
 #endif
 
